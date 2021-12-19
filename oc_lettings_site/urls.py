132cb71c13242ffb9bc3e9a0d3b66ecaf97fa79b
Mathis Cucci <mat.spyder@gmail.com>
2021-12-19 18:33:46 +0100

    rename urls to match new app format

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', include('lettings.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('lettings/', include('lettings.urls'), name="lettings"),
+    path('profiles/', include('profiles.urls'), name="profiles"),
     # path('lettings/', lettings_views.index, name='lettings_index'),
     # path('lettings/<int:letting_id>/', lettings_views.letting, name='letting'),
     # path('profiles/', profiles_views.index, name='profiles_index'),
