c657807ad08d5321f34da7f196628d69b8b58686
Mathis Cucci <mat.spyder@gmail.com>
2021-12-19 23:31:43 +0100

    Add unit tests

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,11 @@
-def test_dummy():
-    assert 1
+from django.urls import resolve, reverse
+
+
+def test_status_code(client):
+    res = client.get(reverse("index"))
+    assert res.status_code == 200
+
+
+def test_title(client):
+    res = client.get(reverse("index"))
+    assert str(res.content).find("<title>Holiday Homes</title>")
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_status_code(client):
+    res = client.get(reverse("profiles:index"))
+    assert res.status_code == 200
+
+@pytest.mark.django_db
+def test_title(client):
+    res = client.get(reverse("profiles:index"))
+    assert str(res.content).find("<title>Profiles</title>")
