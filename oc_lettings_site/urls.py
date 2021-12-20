8d32d0ffcd001ef19091507d5bcdaa0a143b611f
Mathis Cucci <mat.spyder@gmail.com>
2021-12-20 01:12:07 +0100

    Add trigger_error in urls

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 from . import views
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls'), name="lettings"),
     path('profiles/', include('profiles.urls'), name="profiles"),
-    # path('lettings/', lettings_views.index, name='lettings_index'),
-    # path('lettings/<int:letting_id>/', lettings_views.letting, name='letting'),
-    # path('profiles/', profiles_views.index, name='profiles_index'),
-    # path('profiles/<str:username>/', profiles_views.profile, name='profile'),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
